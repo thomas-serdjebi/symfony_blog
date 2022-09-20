8e44950df05e16694d2a8623a4bdebd08e3cdcc3
Thomas <thomas.serdjbei@laplateforme.io>
2022-09-20 18:26:25 +0200

    Bootstrap installation - temp base/index en cours/bug dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 import '../css/app.scss';
+import boostrap from 'bootstrap';
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+
+document.addEventListener('DOMContentLoaded',()=> {
+    enableDropdowns();
+})
+
+const enableDropdowns = () => {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+}
