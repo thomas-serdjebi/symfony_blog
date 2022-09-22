bcecea308fb8bfd2029a2918a1625e448d18fd6f
Thomas <thomas.serdjbei@laplateforme.io>
2022-09-22 15:17:20 +0200

    ARTICLES => création/update/delete/affichage ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepo): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepo->findAll()
         ]);
     }
 }
